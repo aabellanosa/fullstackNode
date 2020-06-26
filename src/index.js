94ab76e8180055b6c1a23a2d7caba171e18cbe4b
aabellanosa <auabellanosa@hotmail.com>
2020-06-26 19:59:05 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,132 @@
+import "dotenv/config";
+
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+mongoose.set("useFindAndModify", false);
+
+app.use(express.static("client"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var Genre = require("../models/genre");
+var Book = require("../models/book");
+
+//connect to mongoose
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+var db = mongoose.connection;
+
+//routes here
+app.get("/", function (req, res) {
+  res.send("Please use /api/books or /api/genres");
+});
+
+app.get("/api/genres", function (req, res) {
+  Genre.getGenres(function (err, genres) {
+    if (err) {
+      throw err;
+    }
+    res.json(genres);
+  });
+});
+
+//add genre
+app.post("/api/genres", function (req, res) {
+  var genre = req.body;
+  Genre.addGenre(genre, function (err, genre) {
+    if (err) {
+      throw err;
+    }
+    res.json(genre);
+  });
+});
+
+//update genre
+app.put("/api/genres/:_id", function (req, res) {
+  var id = req.params._id;
+  var genre = req.body;
+  console.log(`id ${id}`);
+  // console.log(`genre ${genre}`);
+  Genre.updateGenre(id, genre, {}, function (err, genre) {
+    if (err) {
+      throw err;
+    }
+    res.json(genre);
+  });
+});
+
+//delete genre
+app.delete("/api/genres/:_id", function (req, res) {
+  var id = req.params._id;
+  console.log(`id ${id}`);
+  Genre.deleteGenre(id, function (err, genre) {
+    if (err) {
+      throw err;
+    }
+    res.json(genre);
+  });
+});
+
+//retrieve all books
+app.get("/api/books", function (req, res) {
+  Book.getBooks(function (err, books) {
+    if (err) {
+      throw err;
+    }
+    res.json(books);
+  });
+});
+
+//retrieve a book
+app.get("/api/books/:_id", function (req, res) {
+  Book.getBookById(req.params._id, function (err, book) {
+    if (err) {
+      throw err;
+    }
+    res.json(book);
+  });
+});
+
+//create a book
+app.post("/api/books", function (req, res) {
+  var book = req.body;
+  Book.addBook(book, function (err, book) {
+    if (err) {
+      throw err;
+    }
+    res.json(book);
+  });
+});
+
+//update a book
+app.put("/api/books/:_id", function (req, res) {
+  var id = req.params._id;
+  var book = req.body;
+  console.log(`id ${id}`);
+  Book.updateBook(id, book, {}, function (err, book) {
+    if (err) {
+      throw err;
+    }
+    res.json(book);
+  });
+});
+
+//delete book
+app.delete("/api/books/:_id", function (req, res) {
+  var id = req.params._id;
+  console.log(`id ${id}`);
+  Book.deleteBook(id, function (err, book) {
+    if (err) {
+      throw err;
+    }
+    res.json(book);
+  });
+});
+
+app.listen(3000);
+console.log("Running on port 3000...");
